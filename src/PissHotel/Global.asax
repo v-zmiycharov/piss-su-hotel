0940b8d613731b682f32e4bd83eb0401376ea427
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2014-12-09 15:03:13 +0200

    Create project

---
+++ src/PissHotel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PissHotel.MvcApplication" Language="C#" %>
---
+++ src/PissHotel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PissHotel.Startup))]
+namespace PissHotel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
