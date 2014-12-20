ca0e0e9bc2f4f622e981dad54174aee64c4f21e8
Valentin Zmiycharov <valentin.zmiycharov@gmail.com>
2014-12-20 11:38:23 +0200

    Add partial classes for room and album

---
+++ src/PissHotel/Partials/Album.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PissHotel.Models
+{
+    public partial class Album
+    {
+        public List<string> Pictures { get; set; }
+    }
+}
